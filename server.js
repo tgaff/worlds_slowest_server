494b8f977f3f3bf9bf58bbbdf1bd58c999429591
tgaff <tgaff@alumni.nd.edu>
2016-10-03 15:19:04 -0700

    make server super slow

--- server.js
+++ server.js
@@ -1,9 +1,47 @@
 // require express and other modules
 var express = require('express'),
     app = express();
+var Throttle = require('throttle')
+var fs = require("fs");
+var mime = require('mime');
+THROTTLE_SPEED = 20000; // bps
+RESPONSE_DELAY = 3000; // ms
+
 
 // serve static files from public folder
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public'));
+
+// transfers files slowly based on THROTTLE_SPEED with delay RESPONSE_DELAY
+function transferFileSlowly(assetPath, req, res, next) {
+  var fileName = assetPath;
+
+  setTimeout(function() {
+    try {
+        // verify file presence (throws on failure)
+        fs.accessSync(fileName, fs.F_OK);
+        var filestream = fs.createReadStream(fileName);
+        var throttle = new Throttle(THROTTLE_SPEED)
+        console.log("transferring " + fileName + " at " + THROTTLE_SPEED + "bps")
+
+        res.setHeader("Content-Type", mime.lookup(fileName))
+        filestream.pipe(throttle).pipe(res);
+    } catch (e) {
+        // why don't we have proper error classes in node?
+        if (e.message.slice(0,6) === 'ENOENT') {
+          console.log("404 on file: ", fileName);
+          res.sendStatus(404);
+        } else {
+          console.log("unknown error", e);
+          res.sendStatus(400)
+        }
+    }
+
+
+
+  }, RESPONSE_DELAY)
+}
+
+
 
 /************
  * DATABASE *
@@ -20,9 +58,22 @@ app.use(express.static(__dirname + '/public'));
  */
 
 app.get('/', function homepage (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  var file = __dirname + '/views/index.html';
+  transferFileSlowly(file, req,res);
+
+//  res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/css/:fileName', function(req,res,next) {
+  var file = __dirname + '/public/css/' + req.params.fileName
+  transferFileSlowly(file, req,res,next);
+});
+
+
+app.get('/js/:fileName', function(req,res,next) {
+  var file = __dirname + '/public/js/' + req.params.fileName
+  transferFileSlowly(file, req,res,next);
+})
 
 /*
  * JSON API Endpoints
